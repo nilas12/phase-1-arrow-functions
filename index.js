ccd7e26fc611843e486f1904360748dc23194d24
Your Name <senasezer@sena-MacBook-Air.local>
2022-10-22 11:40:56 +0300

    done

---
+++ index.js
@@ -0,0 +1,7 @@
+const divide = (parameter1, parameter2) => parameter1 / parameter2;
+divide(2000,100)
+
+const square = (parameter) => parameter * parameter;
+square(10)
+const add = (parameter1, parameter2) => parameter1 + parameter2;
+add(10,12)
